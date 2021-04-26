cf97ab42a8abe14e9fd3384c6934ddbb8e317f6d
juliaswidron10 <juliaswidron10@gmail.com>
2021-04-26 16:41:59 +0200

    quiz works, kind of....

--- ezone/src/form_steps/quiz.js
+++ ezone/src/form_steps/quiz.js
@@ -1,17 +1,105 @@
 import React from 'react';
-import { Step1 , Step2, Step3 } from './steps.js';
+import { Step1 , Step2, Step3, Step4, Step5, Step6, Step7, Step8 } from './steps.js';
 
 export class Quiz extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            currentStep: 1,
+            hours: 0,
+            stream: false,
+            interests: [],
+            games: [],
+            types: [],
+            content: []
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this._next = this._next.bind(this)
+        this._prev = this._prev.bind(this)
+        // this.handleBack = this.handleBack.bind(this)
+        // this.handleForward = this.handleForward.bind(this)
+    }
+    handleChange(event){
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const {currentStep, hours, stream, interests, games, types, content} = this.state
+        alert(`Boohooo`)
+    }
+    _next() {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep >= 7? 8: currentStep + 1
+        this.setState({
+            currentStep: currentStep
+        })
+    }
+    _prev() {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep <= 1? 1: currentStep - 1
+        this.setState({
+            currentStep: currentStep
+        })
+    }
+
+    previousButton() {
+        let currentStep = this.state.currentStep;
+        if(currentStep !==1){
+          return (
+            <button 
+              className="btn btn-secondary" 
+              type="button" onClick={this._prev}>
+            Previous
+            </button>
+          )
+        }
+        return null;
+      }
+      
+      nextButton(){
+        let currentStep = this.state.currentStep;
+        if(currentStep < 8){
+          return (
+            <button 
+              className="btn btn-primary float-right" 
+              type="button" onClick={this._next}>
+            Next
+            </button>        
+          )
+        }
+        return null;
+      }
+
     render() {
         return(
             <div>
-                <Step1 />
-                <Step2 />
-                <Step3 />
-                <div className='navigationbuttons'>
-                    <h1 className='back'>back</h1>
-                    <h2 className='next'>next</h2>
-                </div>
+                <form onSumit={this.handleSubmit}>
+                    {/* <fieldset> */}
+                        <Step1 currentStep={this.state.currentStep} handleChange={this.hangleChange} />
+                        {/* </fieldset>
+                    <fieldset onSumit={this.handleSubmit}> */}
+                        <Step2 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        <Step3 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        <Step4 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        <Step5 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        {/* </fieldset>
+                    <fieldset> */}
+                        <Step6 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        {/* </fieldset>
+                    <fieldset> */}
+                        <Step7 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        {/* </fieldset>
+                    <fieldset> */}
+                        <Step8 currentStep={this.state.currentStep} handleChange={this.hangleChange}/>
+                        {/* </fieldset> */}
+                        {this.previousButton()}
+        {this.nextButton()}
+                </form>
             </div>
         )
     }
--- ezone/src/form_steps/steps.js
+++ ezone/src/form_steps/steps.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 export class Step1 extends React.Component {
     render() {
-    // if (this.props.currentStep !== 1) { // Prop: The current step
-    //   return null
-    // }
+    if (this.props.currentStep !== 1) { // Prop: The current step
+      return null
+    }
     // The markup for the Step 1 UI
     return(
       <div className="form-group">
-        <form>
             <label htmlFor='hoursplaying' >How many hours do you spend playing in a day?</label>
             <div className='minmaxrange'><p>0</p><p>18+</p></div>
             <input 
@@ -18,6 +17,8 @@ export class Step1 extends React.Component {
             type='range'
             min='0' max='18'
             step='1'
+            value={this.props.hours} // Prop: The email input data
+        onChange={this.props.handleChange} 
             />
             <h1 className="currentaState">10hrs/day</h1>
             <div>
@@ -27,10 +28,10 @@ export class Step1 extends React.Component {
                 id="streaming"
                 name='streaming'
                 type='radio'
+                value={this.props.stream} // Prop: The email input data
+        onChange={this.props.handleChange} 
                 />
             </div>
-            
-        </form>
       </div>
     )}
   }
@@ -38,16 +39,15 @@ export class Step1 extends React.Component {
 
 export class Step2 extends React.Component {
     render() {
-    // if (this.props.currentStep !== 1) { // Prop: The current step
-    //   return null
-    // }
-    // The markup for the Step 1 UI
+    if (this.props.currentStep !== 2) { // Prop: The current step
+      return null
+    }
+    // // The markup for the Step 1 UI
     return(
       <div className="form-group">
           <h1>What areas would you like to improve?</h1>
           <div className='icon-areas'></div>
           <h1>Physical</h1>
-        <form>
             <label htmlFor='sleep'>Sleep</label>
             <input
             type="checkbox"
@@ -88,10 +88,6 @@ export class Step2 extends React.Component {
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input> 
-            
-
-            
-        </form>
       </div>
     )}
   }
@@ -99,16 +95,15 @@ export class Step2 extends React.Component {
 
 export class Step3 extends React.Component {
     render() {
-    // if (this.props.currentStep !== 1) { // Prop: The current step
-    //   return null
-    // }
+    if (this.props.currentStep !== 3) { // Prop: The current step
+      return null
+    }
     // The markup for the Step 1 UI
     return(
       <div className="form-group">
           <h1>What areas would you like to improve?</h1>
           <div className='icon-areas'></div>
           <h1>Mental</h1>
-        <form>
             <label htmlFor='mindset'>Mindset</label>
             <input
             type="checkbox"
@@ -133,23 +128,21 @@ export class Step3 extends React.Component {
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input>
-        </form>
       </div>
     )}
   }
 
 export class Step4 extends React.Component {
     render() {
-    // if (this.props.currentStep !== 1) { // Prop: The current step
-    //   return null
-    // }
+    if (this.props.currentStep !== 4) { // Prop: The current step
+      return null
+    }
     // The markup for the Step 1 UI
     return(
       <div className="form-group">
           <h1>What areas would you like to improve?</h1>
           <div className='icon-areas'></div>
           <h1>Skills</h1>
-        <form>
             <label htmlFor='tactical'>Tactical</label>
             <input
             type="checkbox"
@@ -166,31 +159,196 @@ export class Step4 extends React.Component {
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input>
-             <label htmlFor='psychology'>Physiology</label>
+             <label htmlFor='leadership'>Leadership</label>
             <input
             type="checkbox"
-            value='Physiology'
-            name='psychology'
+            value='Leadership'
+            name='leadership'
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input>
-             <label htmlFor='stress'>Stress</label>
+             <label htmlFor='multitasking'>Multi-tasking</label>
             <input
             type="checkbox"
-            value='Stress'
-            name='stress'
+            value='Multi-tasking'
+            name='multitasking'
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input>
-             <label htmlFor='psychology'>Physiology</label>
+             <label htmlFor='communication'>Communication</label>
             <input
             type="checkbox"
-            value='Physiology'
-            name='psychology'
+            value='Communication'
+            name='communication'
             // checked={this.state.isGoing}
             // onChange={this.handleInputChange}
             ></input>
-        </form>
       </div>
     )}
   }
+
+export class Step5 extends React.Component {
+  render() {
+  if (this.props.currentStep !== 5) { // Prop: The current step
+    return null
+  }
+  // The markup for the Step 1 UI
+  return(
+    <div className="form-group">
+        <h1>What areas would you like to improve?</h1>
+        <div className='icon-areas'></div>
+        <h1>Gaming</h1>
+          <label htmlFor='technology'>Technology</label>
+          <input
+          type="checkbox"
+          value='Technology'
+          name='technology'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+          <label htmlFor='coordination'>Hand/eye coordination</label>
+          <input
+          type="checkbox"
+          value='Hand/eye coordination'
+          name='coordination'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+           <label htmlFor='reaction'>Reaction time</label>
+          <input
+          type="checkbox"
+          value='Reaction time'
+          name='reaction'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+    </div>
+  )}
+}
+
+export class Step6 extends React.Component {
+  render() {
+  if (this.props.currentStep !== 6) { // Prop: The current step
+    return null
+  }
+  // The markup for the Step 1 UI
+  return(
+    <div className="form-group">
+        <label htmlFor='pick-games'>
+        What games do you play?</label>
+          <select id="pick-games"
+          //  value={this.state.value} onChange={this.handleChange}
+           >
+            <option value="CS:GO">CS:GO</option>
+            <option value="Tekken">Tekken</option>
+            <option value="FIFA">FIFA</option>
+            <option value="Super Mario">Super Mario</option>
+            <option value="Fortnite">Fortnite</option>
+            <option value="Call of Duty">Call of Duty</option>
+            <option value="The Sims">The Sims</option>
+            <option value="Mortal Combat">Mortal Combat</option>
+            <option value="Guitar Hero">Guitar Hero</option>
+            <option value="World of Warcfarft">World of Warcraft</option>
+            <option value="Skyrim">Skyrim</option>
+            <option value="Starcraft">Starcraft</option>
+            <option value="Warcraft">Warcraft</option>
+            <option value="League of Legends">League of Legends</option>
+            <option value="Dota">Dota</option>
+            <option value="Valorant">Valorant</option>
+            <option value="Left for dead">Left for dead</option>
+            <option value="Overwatch">Overwatch</option>
+            <option value="PUBG">PUBG</option>
+            <option value="Diablo">Diablo</option>
+            <option value="Minecraft">Minecraft</option>
+          </select>
+          
+        <div className="selected-games">
+          <p className="name-selected">name of the game</p>
+        </div>
+    </div>
+  )}
+}
+
+
+export class Step7 extends React.Component {
+  render() {
+  if (this.props.currentStep !== 7) { // Prop: The current step
+    return null
+  }
+  // The markup for the Step 1 UI
+  return(
+    <div className="form-group">
+        <label htmlFor='pick-games'>
+        What’s your favorite type of game to play?</label>
+          <select id="pick-games"
+          //  value={this.state.value} onChange={this.handleChange}
+           >
+            <option value="FPS">FPS</option>
+            <option value="MMORPG">MMORPG</option>
+            <option value="Sports">Sports</option>
+            <option value="Racing">Racing</option>
+            <option value="Tower defence">Tower defence</option>
+            <option value="RTS">RTS</option>
+            <option value="Simulation">Simulation</option>
+            <option value="Role-playing">Role-playing</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Survival">Survival</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Battle royale">Battle royale</option>
+            <option value="Rhythym">Rhythym</option>
+            <option value="Platform">Platform</option>
+          </select>
+        <div className="selected-types">
+          <p className="type-selected">name of the game</p>
+        </div>
+    </div>
+  )}
+}
+
+
+export class Step8 extends React.Component {
+  render() {
+  if (this.props.currentStep !== 8) { // Prop: The current step
+    return null
+  }
+  // The markup for the Step 1 UI
+  return(
+    <div className="form-group">
+        <h1>What type of contend would you want to recive</h1>
+      <form>
+          <label htmlFor='articles'>Articles</label>
+          <input
+          type="checkbox"
+          value='Articles'
+          name='articles'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+          <label htmlFor='discounts'>Discounts</label>
+          <input
+          type="checkbox"
+          value='Discounts'
+          name='dicounts'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+           <label htmlFor='events'>Online community events</label>
+          <input
+          type="checkbox"
+          value='events'
+          name='events'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+            <label htmlFor='competitions'>Competitions</label>
+          <input
+          type="checkbox"
+          value='Competitions'
+          name='competitions'
+          // checked={this.state.isGoing}
+          // onChange={this.handleInputChange}
+          ></input>
+      </form>
+    </div>
+  )}
+}
