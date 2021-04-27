8fcc4a1bf688e6a9e618257c0af6e775fca902e7
juliaswidron10 <juliaswidron10@gmail.com>
2021-04-27 17:43:18 +0200

    something happened now it works worse than before

---
+++ ezone/src/background/background.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export class Background extends React.Component {
+    render() {
+        return(
+            <div className="backgroundanim">
+                <div className="background">
+                    <h1 className="sentence">IMPROVE YOUR PERFORMANCE</h1>
+                    <div className="container">
+                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
+                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
+                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
+                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
+                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
+                        {/* <div className="solid-frame centered"></div>
+                        <div className="outline-frame centered"></div> */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
