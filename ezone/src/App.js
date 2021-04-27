f934cc58e4ccf1d65c6deca924a32ce7b44e0396
juliaswidron10 <juliaswidron10@gmail.com>
2021-04-27 17:30:12 +0200

    AGAIN

--- ezone/src/App.js
+++ ezone/src/App.js
@@ -1,10 +1,11 @@
-
+import logo from "./logo.svg";
 import "./App.scss";
+import { Background } from "./background/background.js";
 
 function App() {
   return (
     <div className="App">
-
+      <Background />
     </div>
   );
 }
