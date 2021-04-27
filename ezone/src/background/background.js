0270e42ecbc16c218009fe04bd277426c80c2ae4
juliaswidron10 <juliaswidron10@gmail.com>
2021-04-27 18:10:16 +0200

    animation in the background

--- ezone/src/background/background.js
+++ ezone/src/background/background.js
@@ -5,18 +5,13 @@ export class Background extends React.Component {
     render() {
         return(
             <div className="backgroundanim">
-                <div className="background">
-                    <h1 className="sentence">IMPROVE YOUR PERFORMANCE</h1>
-                    <div className="container">
-                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
-                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
-                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
-                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
-                        <h2 className="sentence">IMPROVE YOUR PERFORMANCE</h2>
-                        {/* <div className="solid-frame centered"></div>
-                        <div className="outline-frame centered"></div> */}
-                    </div>
-                </div>
+                <h1 className="bck-text filled sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
+                <h1 className="bck-text empty sentence">Improve your performance</h1>
             </div>
         )
     }
