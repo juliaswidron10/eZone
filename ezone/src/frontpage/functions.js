e25e220e6b13963180b7566584bc6c41879fbaf9
szangyi <alexandra.mszrs@gmail.com>
2021-04-27 11:16:58 +0200

    push bef pull

--- ezone/src/frontpage/functions.js
+++ ezone/src/frontpage/functions.js
@@ -21,17 +21,15 @@ function CommonThree() {
 export { CommonOne, CommonTwo, CommonThree };
 export default CommonComponentsMain;
 
-// export const frontPageButton = document.querySelector(".btn-frontpage");
-// const signupStep1Button = document.querySelector(".btn-signup-step1");
-// const signupStep2Button = document.querySelector(".btn-signup-step2");
-// export const modalStep1 = document.querySelector(".modal-step1");
-// const modalStep2 = document.querySelector(".modal-step2");
+const frontPageButton = document.querySelector(".btn-frontpage");
+const signupStep1Button = document.querySelector(".btn-signup-step1");
+const signupStep2Button = document.querySelector(".btn-signup-step2");
+const modalStep1 = document.querySelector(".modal-step1");
+const modalStep2 = document.querySelector(".modal-step2");
 
-// export function FrontButton() {
-//   frontPageButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     console.log("showmodal1");
-//     modalStep1.removeAttribute("hidden");
-//     modalStep1.classList.add("slideup");
-//   });
-// }
+frontPageButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("showmodal1");
+  modalStep1.removeAttribute("hidden");
+  modalStep1.classList.add("slideup");
+});
