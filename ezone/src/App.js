81d43a5851c0046cf81f43ddc811d5fd8eaf5427
juliaswidron10 <juliaswidron10@gmail.com>
2021-04-27 17:22:37 +0200

    pulling background

--- ezone/src/App.js
+++ ezone/src/App.js
@@ -1,10 +1,11 @@
+import logo from "./logo.svg";
 import "./App.scss";
-import { Quiz } from "./form_steps/quiz.js";
+import { Background } from "./background/background.js";
 
 function App() {
   return (
     <div className="App">
-      <Quiz />
+      <Background />
     </div>
   );
 }
