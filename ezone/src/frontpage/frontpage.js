028ed53c3271627d06d690b0915f7040f413b133
szangyi <alexandra.mszrs@gmail.com>
2021-04-28 23:04:52 +0200

    finalito

--- ezone/src/frontpage/frontpage.js
+++ ezone/src/frontpage/frontpage.js
@@ -3,18 +3,8 @@ import { Modal1 } from "./modals.js";
 import { Modal2 } from "./modals.js";
 
 export class FrontPage extends React.Component {
-  // const frontPageButton = document.querySelector(".btn-frontpage");
-  // const signupStep1Button = document.querySelector(".btn-signup-step1");
-  // const signupStep2Button = document.querySelector(".btn-signup-step2");
-  // const modalStep1 = document.querySelector(".modal-step1");
-  // const modalStep2 = document.querySelector(".modal-step2");
-
   openModal1() {
     console.log("showmodal1");
-    //reset
-    // document.querySelector(".modal-step1").classList.remove("slidedown");
-    // document.querySelector(".modal-step2").classList.remove("slidedown");
-
     document.querySelector(".frontpageBody").classList.add("overlay");
     document.querySelector(".btn-frontpage").classList.add("fadeout");
     document.querySelector(".modal-step1").removeAttribute("hidden");
